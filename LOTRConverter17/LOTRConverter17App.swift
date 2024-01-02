5a9d054804832b1a175f1c06641edb8354be2eac
Nguyen Manh Hung <manhhunghd2000@gmail.com>
2024-01-02 20:37:10 +0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Nguyễn Mạnh Hùng on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
